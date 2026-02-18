73ec28bf26dd66195ffd9448f950b51d239ef5aa
vvmai <vv.mai98@gmail.com>
2026-02-18 10:27:51 -0500

    cnsolidate

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ pg = st.navigation([st.Page("About.py"),
                     st.Page("pages/Slim Jim percentage.py"),
                     st.Page("pages/Plotting my next move.py"),
                     st.Page("pages/Is grad school worth it?.py"),
-                    st.Page("pages/Stream of consciousness.py"),
+                    st.Page("pages/Game of Life.py"),
+                    # st.Page("pages/Stream of consciousness.py"),
                     ])
 
 pg.run()
